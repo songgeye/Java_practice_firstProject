84357553a689d85a8ac35a21264ed93a3904ce68
songgeye <nov.twenty1120dibali@gmail.com>
2025-07-16 23:35:51 +0900

    講義第4回の04_変数と値の取り扱い 簡単な計算を完了

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-        // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.println("Hello and welcome!");
+//        double number1 = 5;
+//        double number2 = 10;
+//        double sum = number1 + number2;
+//        System.out.println(sum);
+//
+//        number1 = sum;
+//
+//        System.out.println(number1 + number2 * (number1 + number2));
+//
+//        number1 = 100;
+//        number2 = 200;
+//
+//        System.out.println(number1 - number2);
+//        System.out.println(number1 * number2);
+//        System.out.println(Math.round(number1 / number2));
+//        System.out.println(number1 % number2);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
-        }
+        //文字列結合と数値計算結果の結合をして
+        //自分の名前 + 自分の年齢 + "歳です"　が出力されるようにする。
+
+        String name = "MatsuganoKengo";
+        int age = 31;
+        System.out.println(name + "は" + age + "歳です");
     }
 }
