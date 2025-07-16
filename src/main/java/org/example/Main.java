43e65e5e4f328df7ed873261568027d17dad906d
songgeye <nov.twenty1120dibali@gmail.com>
2025-07-16 23:49:13 +0900

    不要なコメントアウトを削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,26 +6,6 @@ import java.sql.SQLOutput;
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-//        double number1 = 5;
-//        double number2 = 10;
-//        double sum = number1 + number2;
-//        System.out.println(sum);
-//
-//        number1 = sum;
-//
-//        System.out.println(number1 + number2 * (number1 + number2));
-//
-//        number1 = 100;
-//        number2 = 200;
-//
-//        System.out.println(number1 - number2);
-//        System.out.println(number1 * number2);
-//        System.out.println(Math.round(number1 / number2));
-//        System.out.println(number1 % number2);
-
-        //文字列結合と数値計算結果の結合をして
-        //自分の名前 + 自分の年齢 + "歳です"　が出力されるようにする。
-
         String name = "MatsuganoKengo";
         int age = 31;
         System.out.println(name + "は" + age + "歳です");
