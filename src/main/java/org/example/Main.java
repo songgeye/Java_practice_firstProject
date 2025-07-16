197a4123c85e0df1c1ad9e7150c0ead3ceb45b5e
songgeye <nov.twenty1120dibali@gmail.com>
2025-07-16 23:51:15 +0900

    不要なコメントアウトを削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import java.sql.SQLOutput;
 
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
         String name = "MatsuganoKengo";
