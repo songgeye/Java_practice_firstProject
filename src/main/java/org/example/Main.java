bdf9419a68b74671016a463f8437ba5898fb535b
songgeye <nov.twenty1120dibali@gmail.com>
2025-07-17 20:49:56 +0900

    不要なimport文を削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import java.sql.SQLOutput;
-
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
 //        double number1 = 5;
