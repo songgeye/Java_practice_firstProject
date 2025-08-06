62db7e8de71f636ed0f46b90a5dc1779ae04423d
songgeye <nov.twenty1120dibali@gmail.com>
2025-08-06 21:18:32 +0900

    簡単な計算機プログラムの作成

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,28 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("入力例:");
-    System.out.print("1番目の数字を入力してください: ");
     Scanner sc = new Scanner(System.in);
 
-    int calcNum = sc.nextInt();
-    System.out.println("演算子を入力してください (+, -, *, /): ");
+    System.out.println("入力例:");
+
+    System.out.print("1番目の数字を入力してください: ");
+    int firstInt = sc.nextInt();
+
+    System.out.print("演算子を入力してください (+, -, *, /): ");
+    String operator = sc.next();
+
+    System.out.print("2番目の数字を入力してください: ");
+    int secondInt = sc.nextInt();
+
+    int result = 0;
+    switch (operator) {
+      case "+" -> result = firstInt + secondInt;
+      case "-" -> result = firstInt - secondInt;
+      case "*" -> result = firstInt * secondInt;
+      case "/" -> result = firstInt / secondInt;
+
+    }
+    System.out.println("期待される出力例:");
+    System.out.print("計算結果: " + result);
   }
 }
