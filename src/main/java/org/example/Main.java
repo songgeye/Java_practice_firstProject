8fd52446ca53a987cf438abf3d256d421b550091
songgeye <79154673+songgeye@users.noreply.github.com>
2025-08-15 11:55:32 +0900

    Update Main.java
    不要なコメントアウトを削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
 
   private Scanner scanner;
