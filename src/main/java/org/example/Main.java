4515d1100b6e9886645792d24555e25759b32b34
songgeye <nov.twenty1120dibali@gmail.com>
2025-08-16 23:51:32 +0900

    正規表現を使用して、ハイフン付きの国内携帯電話番号の有効性をチェックするプログラムを作成のためのブランチを作成

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,25 +10,5 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     System.out.println("入力例:");
-
-    System.out.print("1番目の数字を入力してください: ");
-    int firstInt = sc.nextInt();
-
-    System.out.print("演算子を入力してください (+, -, *, /): ");
-    String operator = sc.next();
-
-    System.out.print("2番目の数字を入力してください: ");
-    int secondInt = sc.nextInt();
-
-    int result = 0;
-    switch (operator) {
-      case "+" -> result = firstInt + secondInt;
-      case "-" -> result = firstInt - secondInt;
-      case "*" -> result = firstInt * secondInt;
-      case "/" -> result = firstInt / secondInt;
-
-    }
-    System.out.println("期待される出力例:");
-    System.out.print("計算結果: " + result);
   }
 }
