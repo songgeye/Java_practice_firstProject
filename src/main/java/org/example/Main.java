7c5aaec6580d1f38735e9faf1be40a51ff2e8eac
songgeye <nov.twenty1120dibali@gmail.com>
2025-08-18 22:06:53 +0900

    13-1_【🔰必須】初級理解度チェック_問題3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
 
+  private Scanner sc;
+
+  public Main() {
+    this.sc = new Scanner(System.in);
+  }
+
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Main main = new Main();
+    main.InputManager();
+  }
+
+  public void InputManager() {
     System.out.print("携帯電話番号を入力してください: ");
     String inputNumber = sc.nextLine();
     if (PhoneNumberValidator.isValid(inputNumber)) {
