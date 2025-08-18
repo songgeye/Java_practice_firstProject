ef6aaf97a12718fcdc8067b7cc589bb30c4a4002
songgeye <nov.twenty1120dibali@gmail.com>
2025-08-18 21:35:38 +0900

    13-1_【🔰必須】初級理解度チェック_問題3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,9 @@ public class Main {
     System.out.print("携帯電話番号を入力してください: ");
     String inputNumber = sc.nextLine();
     if (PhoneNumberValidator.isValid(inputNumber)) {
-      System.out.println("有効な携帯電話番号です。");
+      System.out.println(inputNumber + " は有効な携帯電話番号です。");
     } else {
-      System.out.println("無効な携帯電話番号です。ハイフンを含まない形式は無効です。");
+      System.out.println(inputNumber + " は無効な携帯電話番号です。");
     }
     sc.close();
   }
